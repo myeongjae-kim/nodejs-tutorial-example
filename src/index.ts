4e17bfc65f5b6b221490d858ebda17d8072ae631
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2022-03-27 14:04:52 +0900

    [#17] use inversify with decorator

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,40 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { ApplicationByMobx } from "./ApplicationByMobx";
-import { ApplicationByRedux } from "./ApplicationByRedux";
+import { Container } from "inversify";
 import { ApplicationByStateManager } from "./ApplicationByStateManager";
 import { createApplicationContext } from "./applicationContext";
+import { Constants } from "./Constants";
 
 const context = createApplicationContext();
 
+/*
 const applicationByStateManager = new ApplicationByStateManager(
   context.stateManager,
   context.articleQueryViewController,
   context.articleCommandViewController
 );
+*/
+
+const bind = (container: Container) => {
+  container
+    .bind(Constants.SERVICE_IDS.StateManager)
+    .toConstantValue(context.stateManager);
+  container
+    .bind(Constants.SERVICE_IDS.ArticleQueryViewController)
+    .toConstantValue(context.articleQueryViewController);
+  container
+    .bind(Constants.SERVICE_IDS.ArticleCommandViewController)
+    .toConstantValue(context.articleCommandViewController);
+  container
+    .bind(Constants.SERVICE_IDS.ApplicationByStateManager)
+    .to(ApplicationByStateManager);
+};
+
+// create IoC Container and bind Constants.SERVICE_IDS to instances or classes
+const container = new Container();
+bind(container);
+
+// get the application
+const applicationByStateManager = container.get<ApplicationByStateManager>(
+  Constants.SERVICE_IDS.ApplicationByStateManager
+);
+
 applicationByStateManager.run();
-
-const applicationByRedux = new ApplicationByRedux(
-  context.store,
-  context.articleQueryViewController,
-  context.articleCommandViewController
-);
-// applicationByRedux.run();
-
-const applicationByMobx = new ApplicationByMobx(
-  context.mobxRootState,
-  context.articleQueryViewController,
-  context.articleCommandViewController
-);
-// applicationByMobx.run();
