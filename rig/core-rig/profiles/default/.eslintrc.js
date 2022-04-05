0069bfb90fd52f9d159916088ef1a35ce95272bb
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2022-04-05 23:28:49 +0900

    [#18] applying misc settings. fix for eslint

--- rig/core-rig/profiles/default/.eslintrc.js
+++ rig/core-rig/profiles/default/.eslintrc.js
@@ -1,10 +1,7 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
+    "@rushstack/eslint-config/profile/node",
+    "@rushstack/eslint-config/mixins/friendly-locals",
   ],
 };
